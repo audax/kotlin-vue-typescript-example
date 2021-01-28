393ee1e81ad34395627dd0b445d58d8c7d2365de
Jens Kadenbach <jens.kadenbach@smartsteuer.de>
2021-01-28 08:30:03 +0100

    Test commit 2

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("multiplatform") version "1.4-M3" apply false
     id("com.github.node-gradle.node") version "2.2.0" apply false
 }
+// a bad change.
 repositories {
     mavenCentral()
     maven {
